10c683099ed74a1e2b95199b7782ec195605d4c1
Mohamed Farouk <admiralmoha@gmail.com>
2020-09-06 19:22:19 +0200

    [ADD] movies UI & test view

--- app/site/routes.py
+++ app/site/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
-site = Blueprint('site', __name__)
+from app.services import ghibli
+
+site = Blueprint('site', __name__, template_folder='templates')
 
 
 @site.route('/movies/')
 def get_movies():
-    return jsonify({'ghibli': 'movies'})
+    movies = ghibli.get_movies_with_people_json()
+
+    return render_template('base.html', movies=movies)
--- testing/test_site.py
+++ testing/test_site.py
@@ -3,10 +3,9 @@ import json
 
 def test_get_movies(app, client):
 
+    # tests movies view html
     res = client.get('/movies/')
 
     assert res.status_code == 200
-
-    expected = {'ghibli': 'movies'}
-
-    assert json.loads(res.get_data()) == expected
+    assert 'text/html' in res.headers['content-type']
+    assert int(res.headers['content-length']) > 0
