021f0dc4619b00aef06c17d4d39bb138016096fe
Mohamed Farouk <admiralmoha@gmail.com>
2020-09-06 20:20:29 +0200

    [ADD] Caching : cache results for 1 minute

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,18 @@ from flask import Flask
 from .site.routes import site
 from .api.routes import api
 
+from .services.cache import cache
+
 
 def create_app():
 
     app = Flask(__name__)
 
+    # init cache for app
+    app.config['CACHE_TYPE'] = 'simple'
+    cache.init_app(app)
+
+    # register site & api blueprints
     app.register_blueprint(site)
     app.register_blueprint(api)
 
--- app/site/routes.py
+++ app/site/routes.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, jsonify, render_template
 
 from app.services import ghibli
+from app.services.cache import cache
 
 site = Blueprint('site', __name__, template_folder='templates')
 
 
 @site.route('/movies/')
+@cache.cached(timeout=60)  # cache results for 1 minute
 def get_movies():
     movies = ghibli.get_movies_with_people_json()
 
